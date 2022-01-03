22953d23c6530ddf7a34297b3cccaf0f0a6def48
Neowise <verve.neowise@gmail.com>
2022-01-03 15:29:52 +0500

    complete bot

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const bot = new TelegramBot(process.env.token, { polling: true })
 bot.on('polling_error', console.log)
 
 const steps = {
-    START: 0, NAME: 1, GENDER: 2, BIRTHDAY: 3, PHONE: 4, EMAIL: 5, ADDRESS: 6, COMPLETE: 7
+    START: 0, NAME: 1, GENDER: 2, PHONE: 4, EMAIL: 5, ADDRESS: 6, COMPLETE: 7
 }
 
 function createUser(id) {
@@ -24,7 +24,7 @@ function createUser(id) {
 
 const users = {}
 // id : user
-
+// any text
 bot.on('message', (msg) => {
 
     let user = findUser(msg)
@@ -36,22 +36,40 @@ bot.on('message', (msg) => {
         if (user.step === steps.NAME) {
             onName(msg)
         }
+        else if (user.step === steps.EMAIL) {
+            onEmail(msg)
+        }
+        else if (user.step === steps.ADDRESS) {
+            onAddress(msg)
+        }
     }
     else {
         bot.sendMessage(msg.chat.id, '/start bosing!')
     }
 })
-
+// knopkalar
 bot.on("callback_query", (query) => {
-    bot.sendMessage(msg.chat.id, '!! Uzur, bot toligligicha ishlamaydi!')
+    let user = findUser(query.message)
+
+    if (user) {
+        if (user.step === steps.GENDER) {
+            onGender(query.message, query.data)
+        }
+    }
 })
 
 bot.on("contact", (msg, metadata) => {
-    bot.sendMessage(msg.chat.id, '!!Uzur, bot toligligicha ishlamaydi!')
+    let user = findUser(msg)
+
+    if (user) {
+        if (user.step === steps.PHONE) {
+            onContact(msg, msg.contact)
+        }
+    }
 })
 
 function onStart(msg) {
-    let user = authorizeUser(msg)
+    let user = authorizeUser(msg) // 885671235
     requireName(user, msg)
 }
 
@@ -62,6 +80,35 @@ function onName(msg) {
     requireGender(user, msg)
 }
 
+function onGender(msg, gender) {
+
+    let user = findUser(msg)
+    user.gender = gender
+
+    requireContact(user, msg)
+}
+
+function onContact(msg, contact) {
+    let user = findUser(msg)
+    user.phone = contact
+
+    requireEmail(user, msg)
+}
+
+function onEmail(msg) {
+    let user = findUser(msg)
+    user.email = msg.text
+
+    requireAddress(user, msg)
+}
+
+function onAddress(msg) {
+    let user = findUser(msg)
+    user.address = msg.text
+
+   complete(user, msg)
+}
+
 function requireName(user, msg) {
     bot.sendMessage(msg.chat.id, "Ismingizni kiriting")
     user.step = steps.NAME
@@ -70,14 +117,39 @@ function requireName(user, msg) {
 function requireGender(user, msg) {
     let genders = {
         inline_keyboard : [
-            [ {text: "Male", callback_data: "male"}, {text: "Female", callback_data: "female"} ]
+            [ { text: "Male", callback_data: "male" }, { text: "Female", callback_data: "female" } ]
         ]
     }
     bot.sendMessage(msg.chat.id, "Genderni tanlang:", { reply_markup: genders })
     user.step = steps.GENDER
 }
 
-function findUser(msg) {
+function requireContact(user, msg) {
+    bot.sendMessage(msg.chat.id, "Kontaktingizni yuboring", { reply_markup: {
+        keyboard: [
+            [ { text: "U+1F4DE Contact yuborish", request_contact: true } ]
+        ],
+        resize_keyboard: true,
+    }})
+
+    user.step = steps.PHONE
+}
+
+function requireEmail(user, msg) {
+    bot.sendMessage(msg.chat.id, "Email ni kiriting:", { reply_markup: { remove_keyboard: true }})
+    user.step = steps.EMAIL
+}
+
+function requireAddress(user, msg) {
+    bot.sendMessage(msg.chat.id, "Addressingizni kiriting:")
+    user.step = steps.ADDRESS
+}
+
+function complete(user, msg) {
+    bot.sendMessage(process.env.adminID, "Siz royhatdan otdingiz\n" + JSON.stringify(user))
+}
+
+function findUser(msg) { // user | undefined
     return users[msg.chat.id]
 }
 
